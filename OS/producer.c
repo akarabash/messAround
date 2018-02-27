ce361f30c53fb205df9f79d9ed8584c99f3d730f
akarabash <31858316+akarabash@users.noreply.github.com>
2018-02-26 21:34:46 -0700

    Update producer.c

--- OS/producer.c
+++ OS/producer.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <errno.h>
 
 int main()
 {
