3891f28fb64b32583e124bdaa93fc781bddb3fdf
akarabash <31858316+akarabash@users.noreply.github.com>
2018-02-26 21:15:28 -0700

    added producer and consumer files

---
+++ OS/consumer.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+
+int main(){
+	
+	char arrays[64];
+  	char *name = arrays; 	// file name
+  	printf("Enter Key \n");
+  	scanf("%s", name);
+
+  	const int shm_size = 4096;
+
+  	const char *message0 = "Amin ";
+  	const char *message1 = "February";
+  	const char *message_end = "\n";
+
+  	shm_fd = shm_open(name, O_RDWR, 0666);
+  	if(shm_fd == -1){
+  		printf("Shared memory failed\n")
+  		exit(1);
+  	}
+
+  	shm_base = mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  	if(shm_fd == -1){
+  		exit(1);
+  	}
+
+  	/* check to see if anything written to shared memory */
+  	if(shm_base == '\0'){
+  		/*read from the mapped shared memory segment */
+  		printf("%s", shm_base);
+
+  		/* write to shared memory segment */
+		ptr = shm_base;
+  		ptr += sprintf(ptr, "%s", message0);
+  		ptr += sprintf(ptr, "%s", message1);
+  		ptr += sprintf(ptr, "%s", message_end);
+
+  	}
+
+  	
+
+  	/*read from the mapped shared memory segment */
+  	//printf("%s", shm_base);
+
+
+
+
+  	/* write to shared memory segment */
+	ptr = shm_base;
+  	ptr += sprintf(ptr, "%s", message0);
+  	ptr += sprintf(ptr, "%s", message1);
+  	ptr += sprintf(ptr, "%s", message_end);
+
+
+
+  	return 0;
+
+
+}
---
+++ OS/producer.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+int main()
+{
+	const int shm_size = 4096; //file size
+	const char *name = "OS";  //file name
+
+	const char *message0 = "Studying ";
+  	const char *message1 = "Operating Systems ";
+  	const char *message2 = "Is Fun!";
+  	const char *msg_end  = "\n";
+
+
+	int shm_fd;
+	char *shm_base;
+	char *ptr;
+
+	/* create shared memory segment like it is a file */
+	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+	if(shm_fd == -1){
+		printf("prod: Shared memory failed: %s\n", strerror(errno));
+    	exit(1);
+	}
+
+	/* configure size of the shared memory segment */
+	ftruncate(shm_fd, shm_size);
+
+	/* map shared memory segment to address space of process */
+	shm_base = mmap(0, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if (shm_base == MAP_FAILED) {
+		printf("Map failed\n");
+		exit(1);
+	}
+	/* write to shared memory segment */
+	ptr = shm_base;
+  	ptr += sprintf(ptr, "%s", "");
+  
+  	return 0;
+  }
+
+
+
+
+
+
+
+}
+
